3275ef8434d3acf550d081ff9f680ead863310b7
D7703_02 <d7703_02@D7703-02ui-iMac.local>
2018-03-14 15:54:06 +0900

    Change

--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ var a = 1
+    @IBOutlet weak var lblColor: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor .yellow
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btChange(_ sender: UIButton) {
+        switch a {
+        case 1:
+            view.backgroundColor = UIColor.yellow
+            a += 1
+            lblColor.text = "yellow"
+        case 2:
+            view.backgroundColor = UIColor.brown
+            a += 1
+            lblColor.text = "brown"
+        default:
+            view.backgroundColor = UIColor.blue
+            a = 1
+            lblColor.text = "blue"
+            <#code#>
+        }
+    }
+    
 }
 
